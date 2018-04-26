0e162239d50dd794baa5ce652cc7d0dacd38cdfc
Stavroula Guerrero <4stacyg@optonline.net>
2018-04-26 16:03:08 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
+def introduction(name)
+  puts "Hi,my name is #{name}."
+end
+
 def introduction_with_language(name)
-  puts "Hi,my name is #{name} and I am learning to progrmam in #{language}."
+  puts "Hi my name is #{name} and I am learning to progrmam in #{language}."
 end
